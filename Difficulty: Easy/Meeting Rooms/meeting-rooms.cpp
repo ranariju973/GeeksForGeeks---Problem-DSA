a10698662aa72338b82d45e2cf7909fa8c16fd1e
Riju Rana <ranariju973@gmail.com>
2026-02-16 15:35:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        sort(arr.begin(), arr.end());
+        for(int i = 0; i < arr.size() - 1; i++ ) {
+            if(arr[i][1] > arr[i+1][0]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};

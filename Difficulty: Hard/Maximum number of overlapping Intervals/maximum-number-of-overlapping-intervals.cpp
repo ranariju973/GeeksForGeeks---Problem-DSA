c150263aa2326cddfdc7de03d1f5e18ac55e75c4
Riju Rana <ranariju973@gmail.com>
2026-02-17 09:32:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum number of overlapping Intervals/maximum-number-of-overlapping-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int overlapInt(vector<vector<int>> &arr) {
+        
+        int n = arr.size();
+        vector<int>start(n),end(n);
+        
+        for(int i = 0; i < arr.size(); i++) {
+            start[i] = arr[i][0];
+            end[i] = arr[i][1];
+        }
+        
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        
+        int i = 0, j = 0;
+        int overlap = 0, maxoverlap = 0;
+        
+        while(i < n && j < n) {
+            if(start[i] <= end[j]){
+                overlap++;
+                maxoverlap = max(maxoverlap, overlap);
+                i++;
+            } else {
+                overlap--;
+                j++;
+            }
+        }
+        
+        return maxoverlap;
+    }
+};
